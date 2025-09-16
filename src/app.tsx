a3f098f779c4b28e9994d30ced05c24b66896830
Ryan Delaney <vitineth@gmail.com>
2025-09-16 10:50:37 +0100

    Remove antlr version of the log formatter
    This removes the route and leaves only the new ones which is no longer marked as legacy

--- src/app.tsx
+++ src/app.tsx
@@ -5,32 +5,29 @@ import { SQLFormatter } from "./lang/sql.tsx";
 import { PromQLFormatter } from "./lang/promql.tsx";
 import { LogFormatter } from "./lang/log.tsx";
 import { DiffPane } from "./lang/diff.tsx";
-import { LogAntlrFormatter } from "./lang/log-antlr.tsx";
 import { ErrorReport } from "./components/error-report.tsx";
 
-
 export function App() {
-	return (<>
-		<ErrorReport/>
-		<TabbedPane>
-			<Tab id={ 'json' } title={ 'JSON Formatter' } keybind={ 'j' }>
-				<JSONFormatter/>
-			</Tab>
-			<Tab id={ 'sql' } title={ 'SQL Formatter' } keybind={ 's' }>
-				<SQLFormatter/>
-			</Tab>
-			<Tab id={ 'prom' } title={ 'MetricsQL Formatter' } keybind={ 'm' }>
-				<PromQLFormatter/>
-			</Tab>
-			<Tab id={ 'diff' } title={ 'Difference' } keybind={ 'd' }>
-				<DiffPane/>
-			</Tab>
-			<Tab id={ 'log' } title={ 'Log Formatter (legacy)' } keybind={ 'l' } localStorageGate={ 'log:antlr' } gateInverted>
-				<LogFormatter/>
-			</Tab>
-			<Tab id={ 'antlr' } title={ 'Log Formatter' } keybind={ 'a' } localStorageGate={ 'log:antlr' }>
-				<LogAntlrFormatter/>
-			</Tab>
-		</TabbedPane>
-	</>)
+  return (
+    <>
+      <ErrorReport />
+      <TabbedPane>
+        <Tab id={"json"} title={"JSON Formatter"} keybind={"j"}>
+          <JSONFormatter />
+        </Tab>
+        <Tab id={"sql"} title={"SQL Formatter"} keybind={"s"}>
+          <SQLFormatter />
+        </Tab>
+        <Tab id={"prom"} title={"MetricsQL Formatter"} keybind={"m"}>
+          <PromQLFormatter />
+        </Tab>
+        <Tab id={"diff"} title={"Difference"} keybind={"d"}>
+          <DiffPane />
+        </Tab>
+        <Tab id={"log"} title={"Log Formatter"} keybind={"l"}>
+          <LogFormatter />
+        </Tab>
+      </TabbedPane>
+    </>
+  );
 }
